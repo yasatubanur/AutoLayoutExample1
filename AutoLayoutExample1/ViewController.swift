d21154648dac6d8a8ce77a38e293793a292f3c91
Tuba Nur <yasatubanur@gmail.com>
2022-03-08 19:10:16 +0300

    Initial Commit

---
+++ AutoLayoutExample1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayoutExample1
+//
+//  Created by Tuba Nur YAŞA on 8.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
